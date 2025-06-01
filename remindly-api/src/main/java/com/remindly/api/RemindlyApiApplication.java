400b662abdf175de16ae388def3448afb7c38252
shash236 <23shashwatik1999@gmail.com>
2025-06-01 20:22:32 +0530

    First Commit

---
+++ remindly-api/src/main/java/com/remindly/api/RemindlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.remindly.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindlyApiApplication.class, args);
+	}
+
+}
